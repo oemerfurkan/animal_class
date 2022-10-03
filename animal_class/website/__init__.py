cd7f26ddc130cdd62d4d3aacbb953695f4a2ab09
oemerfurkan <113520172+oemerfurkan@users.noreply.github.com>
2022-10-03 23:55:21 +0300

    Update __init__.py

--- animal_class/website/__init__.py
+++ animal_class/website/__init__.py
@@ -5,9 +5,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'x831jo239nx903109jxğ02jx'
 
     from .views import views
-    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
 
     return app
